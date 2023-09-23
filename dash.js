93251e1bde479ecfbf940817c2b9170fcd467caf
Prashareet Choudhury <prashareet@gmail.com>
2023-09-23 15:10:15 +0530

    Update dash.js

--- dash.js
+++ dash.js
@@ -1,9 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggleButton");
-  const menu = document.querySelector(".menu");
+// let menu_icon_box=document.querySelector(".menu_icon_box");
+//       let menu=document.querySelector(".menu");
 
-  toggleButton.addEventListener("click", function () {
-    // Toggle the visibility of the menu
-    menu.classList.toggle("hidden");
-  });
-});
+//         menu_icon_box.onclick=function()
+//         {
+//              menu_icon_box.classList.toggle("active");
+//              menu.classList.toggle("active");
+//         }
+
+//         document.onclick=function(e){
+//           if(!menu_icon_box.side-bar(e.target) && !menu.side-bar(e.target))
+//           {
+//                 menu_icon_box.classList.remove("active");
+//                 menu.classList.remove("active");
+//           }
+//         }
+let menu_icon_box = document.querySelector(".menu_icon_box");
+let menu = document.querySelector(".menu");
+
+function toggleMenu() {
+    menu_icon_box.classList.toggle("active");
+    menu.classList.toggle("active-menu");
+}
+
+document.onclick = function (e) {
+    if (!menu_icon_box.contains(e.target) && !menu.contains(e.target)) {
+        menu_icon_box.classList.remove("active");
+        menu.classList.remove("active-menu");
+    }
+};
