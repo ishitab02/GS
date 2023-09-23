fcd33103cf256ae4440316f5dacf108ed48f8acb
ayan324 <145318771+ayan324@users.noreply.github.com>
2023-09-23 06:43:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+var express=require("express");
+var bodyParser=require("body-parser");
+ 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://ayanroy:VB6FFuTDcv48dYO7@cluster0.zaupyyk.mongodb.net/');
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+ 
+var app=express()
+  
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+app.post('/sign_up', function(req,res){
+    var first_name = req.body.first_name;
+    var middle_name = req.body.middle_name;
+    var last_name = req.body.last_name;
+    var email =req.body.email;
+    var pass = req.body.password;
+    var phone =req.body.phone;
+    var sex= req.body.sex;
+    var state= req.body.state;
+ 
+    var data = {
+        "first_name": first_name,
+        "middle_name": middle_name,
+        "last_name":last_name,
+        "email":email,
+        "password":pass,
+        "phone":phone,
+        "sex": sex,
+        "state":state
+    }
+    db.collection('details').insertOne(data,function(err, collection){
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+             
+    });
+         
+    return res.status(200);
+})
+ 
+ 
+app.get('/',function(req,res){
+res.set({
+    'Access-control-Allow-Origin': '*'
+    });
+return res.redirect('index.html');
+}).listen(3000)
+ 
+ 
+console.log("server listening at port 3000");
